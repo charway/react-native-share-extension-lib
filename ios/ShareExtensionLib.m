4df25c29eca8e1142b3fcb7fb37d8384275d3989
guchengwei <guchengwei@meituan.com>
2020-02-02 20:18:46 +0800

    update

--- ios/ShareExtensionLib.m
+++ ios/ShareExtensionLib.m
@@ -23,6 +23,14 @@ NSExtensionContext* extensionContext;
 
 RCT_EXPORT_MODULE()
 
+- (dispatch_queue_t)methodQueue {
+    return dispatch_get_main_queue();
+}
+
++ (BOOL)requiresMainQueueSetup {
+    return YES;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
